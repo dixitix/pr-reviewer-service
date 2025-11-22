4d11fdc4a909ba4554ed8b9c693e528e4917fd26
Diana Tikhonova <dixi.tix@gmail.com>
2025-11-22 20:08:41 +0300

    feat: implement /pullRequest/create endpoint

--- internal/http/pullrequest_handlers.go
+++ internal/http/pullrequest_handlers.go
@@ -2,7 +2,14 @@
 // для HTTP-слоя сервиса назначения ревьюеров.
 package httpserver
 
-import "net/http"
+import (
+	"encoding/json"
+	"errors"
+	"net/http"
+
+	"github.com/dixitix/pr-reviewer-service/internal/domain"
+	"github.com/dixitix/pr-reviewer-service/internal/service"
+)
 
 // handlePullRequestCreate обрабатывает создание нового PR.
 func (h *Handler) handlePullRequestCreate(w http.ResponseWriter, r *http.Request) {
@@ -10,7 +17,65 @@ func (h *Handler) handlePullRequestCreate(w http.ResponseWriter, r *http.Request
 		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
 		return
 	}
-	// TODO handling route
+
+	var req CreatePullRequestRequest
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		http.Error(w, "invalid JSON body", http.StatusBadRequest)
+		return
+	}
+
+	if req.PullRequestID == "" || req.PullRequestName == "" || req.AuthorID == "" {
+		http.Error(w, "pull_request_id, pull_request_name and author_id are required", http.StatusBadRequest)
+		return
+	}
+
+	ctx := r.Context()
+
+	if h.logger != nil {
+		h.logger.Printf(
+			"handlePullRequestCreate: pr_id=%s author_id=%s",
+			req.PullRequestID,
+			req.AuthorID,
+		)
+	}
+
+	pr, err := h.svc.CreatePullRequest(
+		ctx,
+		domain.PullRequestID(req.PullRequestID),
+		req.PullRequestName,
+		domain.UserID(req.AuthorID),
+	)
+	if err != nil {
+		switch {
+		case errors.Is(err, service.ErrPullRequestAlreadyExists):
+			writeJSONError(w, http.StatusConflict, errorCodePRExists, "pull_request_id already exists")
+			return
+
+		case errors.Is(err, service.ErrNotFound):
+			writeJSONError(w, http.StatusNotFound, errorCodeNotFound, "author or team not found")
+			return
+
+		default:
+			if h.logger != nil {
+				h.logger.Printf("handlePullRequestCreate: CreatePullRequest error: %v", err)
+			}
+			http.Error(w, "internal server error", http.StatusInternalServerError)
+			return
+		}
+	}
+
+	resp := PullRequestEnvelope{
+		PullRequest: mapPullRequestDomainToDTO(pr),
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+
+	if err := json.NewEncoder(w).Encode(resp); err != nil {
+		if h.logger != nil {
+			h.logger.Printf("handlePullRequestCreate: failed to write response: %v", err)
+		}
+	}
 }
 
 // handlePullRequestMerge обрабатывает merge PR.
