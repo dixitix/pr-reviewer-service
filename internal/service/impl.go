e8760a2f16f4aa21f8cd09b7f82c6b9174ea6b3a
Diana Tikhonova <dixi.tix@gmail.com>
2025-11-23 12:08:53 +0300

    feat: add service implementation skeleton

---
+++ internal/service/impl.go
@@ -0,0 +1,32 @@
+// Package service содержит реализацию бизнес-логики сервиса назначения ревьюеров.
+package service
+
+import (
+	"math/rand"
+	"time"
+
+	"github.com/dixitix/pr-reviewer-service/internal/repository"
+)
+
+// service — реализация интерфейса Service.
+type service struct {
+	teamRepo        repository.TeamRepository
+	userRepo        repository.UserRepository
+	pullRequestRepo repository.PullRequestRepository
+
+	rnd *rand.Rand
+}
+
+// NewService создаёт новый экземпляр Service.
+func NewService(
+	teamRepo repository.TeamRepository,
+	userRepo repository.UserRepository,
+	pullRequestRepo repository.PullRequestRepository,
+) Service {
+	return &service{
+		teamRepo:        teamRepo,
+		userRepo:        userRepo,
+		pullRequestRepo: pullRequestRepo,
+		rnd:             rand.New(rand.NewSource(time.Now().UnixNano())),
+	}
+}
